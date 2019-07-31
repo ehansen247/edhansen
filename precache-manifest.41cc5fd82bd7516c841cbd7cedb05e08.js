4687dee1211b6d5bddff5e091f263f65ce4cb5ea
Eric Hansen <erichansen@college.harvard.edu>
2019-07-31 13:58:15 +0100

    Updates

---
+++ precache-manifest.41cc5fd82bd7516c841cbd7cedb05e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c0b4735c947b7a509bc30030a359e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3adbb8936ee49d2492b7",
+    "url": "/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "448cca710fff3d1aac5f",
+    "url": "/static/css/main.f7985c4a.chunk.css"
+  },
+  {
+    "revision": "3adbb8936ee49d2492b7",
+    "url": "/static/js/2.8d7ace79.chunk.js"
+  },
+  {
+    "revision": "448cca710fff3d1aac5f",
+    "url": "/static/js/main.577d8da8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a7912385cafb85e5ff781d66bee4906d",
+    "url": "/static/media/Photo.a7912385.png"
+  }
+]);
