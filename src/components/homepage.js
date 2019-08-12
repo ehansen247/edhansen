c6a30ae3df8d3acc8c924c4e38e8bf1c1c543fb7
ehansen247 <ehansen247@gmail.com>
2019-08-12 02:29:56 +0100

    Removed faulty projects link

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -5,17 +5,17 @@ class Homepage extends Component {
     render () {
         return (
             <div>
-                <p>Hi, I'm Eric, a rising sophomore at Harvard College studying Applied Mathematics
+                <p> Hi, I'm Eric, a rising sophomore at Harvard College studying Applied Mathematics
                     and Computer Science. I'm currently studying abroad at the London School
                     of Economics. If you'd like to see a portfolio of my programming projects,
                     feel free to check out my <a href="https://github.com/ehansen247">GitHub</a>, or
-                    the descriptions and links under the <a href="./projects">Projects</a> tab on this site. Outside of programming, my interests include being a part of
+                    the descriptions and links under the Projects tab on this site. Outside of programming,
+                    my interests include being a part of
                     the <a href="https://www.gocrimson.com/sports/sailing/index">Harvard Varsity Sailing Team</a> and
                     the <a href="http://business.thecrimson.com/">Crimson Business Board</a>.
-                    I'll be studying Computer
-                    Science along with Mathematics, Statistics, and Economics over the next three
-                    years, and am looking to work in a Software Development role during the
-                    next summer.
+                    I'll be studying Computer Science along with Mathematics, Statistics,
+                    and Economics over the next three years, and am looking to work in a Software
+                    Development role during the next summer.
                 </p>
                 <p>
                     Feel free to contact me at ehansen247@gmail.com or through social media if
