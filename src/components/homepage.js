749b282d4525c348bf886ab099154142bc46b32f
Eric Hansen <43194881+ehansen247@users.noreply.github.com>
2024-02-18 14:52:10 -0500

    Update homepage.js

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -5,19 +5,12 @@ class Homepage extends Component {
     render () {
         return (
             <div>
-                <p> Hi, I'm Eric, a sophomore at Harvard College studying Computer Science and
-                    and Applied Math. If you'd like to see a portfolio of my programming projects,
-                    feel free to check out my <a href="https://github.com/ehansen247">GitHub</a>, or
-                    the descriptions and links under the Projects tab on this site.
-                    Outside of programming, my interests include being a part of
-                    the <a href="https://www.gocrimson.com/sports/sailing/index">Harvard Varsity Sailing Team</a> and
-                    the <a href="http://business.thecrimson.com/">Crimson Business Board</a>.
-                    I'll be studying Computer Science along with Mathematics, Statistics,
-                    and Economics over the next three years.
+                <p> Initially created this site as to share a few personal programming projects.
+                    Feel free to check out my <a href="https://github.com/ehansen247">GitHub</a>, or
+                    the descriptions and links under the Projects tab on this site to see these.
                 </p>
                 <p>
-                    Feel free to contact me at ehansen247@gmail.com or through social media if
-                    you have any questions about me, this site, or anything else.
+                    Contact me at ehansen247@gmail.com.
                 </p>
             </div>
         );
